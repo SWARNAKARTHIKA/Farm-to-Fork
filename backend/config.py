48a716004aac57b6236b809154b0e537e56d7b85
Soumya Renganathen <136829848+soumya2k4nathen@users.noreply.github.com>
2025-04-06 00:29:41 +0530

    removed config.py

--- backend/config.py
+++ backend/config.py
@@ -1,6 +1,1 @@
-import firebase_admin
-from firebase_admin import credentials
 
-if not firebase_admin._apps:
-    cred = credentials.Certificate("serviceAccountKey.json")
-    firebase_app = firebase_admin.initialize_app(cred)
